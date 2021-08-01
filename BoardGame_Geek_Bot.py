c59f09d63f237b7360142913cf9f722bd67d494b
riccardo berto <ricki1993.scorpione@gmail.com>
2021-08-01 08:42:09 +0200

    Working 0.2
    Missing stats by player

--- BoardGame_Geek_Bot.py
+++ BoardGame_Geek_Bot.py
@@ -27,10 +27,15 @@ logger = logging.getLogger(__name__)
 
 games_list = list()
 
-FIRST, SECOND = range(2)
-ONE, TWO, THREE, FOURTH = range(4)
+FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH = range(6)
+ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN = range(7)
 
 tmp_game = [' ', ' ']
+
+
+def dyn_string(index, val, max):
+    if index > max: return ''
+    return '{} volte {}°, '.format(val[index - 1], index) + dyn_string(index + 1, val, max)
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -41,35 +46,38 @@ def check_game(update: Update, context: CallbackContext):
     query = update.callback_query
     query.answer()
     query.message.reply_text('Inserire Nome Del Gioco')
-    return TWO
-    # update.message.reply_text('Inserire Nome del gioco')
+    return ONE
 
 
 def check_size(update: Update, context: CallbackContext):
     tmp_game[0] = update.message.text
     update.message.reply_text('Inserire giocatori massimi')
-    return THREE
+    return TWO
 
 
 def insert_game(update: Update, context: CallbackContext):
+    global tmp_game
     tmp_game[1] = update.message.text
     if tmp_game[0] not in [i.get_Name() for i in games_list]:
         games_list.append(Game(tmp_game[0], tmp_game[1]))
         update.message.reply_text('Gioco Aggiunto!')
     else:
         update.message.reply_text('Gioco Già presente!')
+
+    tmp_game = [' ', ' ']
     return ConversationHandler.END
 
 
 def commands(update: Update, context: CallbackContext):
     keyboard = [
         [InlineKeyboardButton("Add Game", callback_data=str(FIRST))],
-        [InlineKeyboardButton("Get Stats", callback_data=str(SECOND))]
-
+        [InlineKeyboardButton("Get Stats", callback_data=str(SECOND))],
+        [InlineKeyboardButton("Update Game", callback_data=str(FIFTH))],
+        [InlineKeyboardButton("Show Stored Games", callback_data=str(SIXTH))]
     ]
 
-    reply_markup = InlineKeyboardMarkup(keyboard, one_time_keyboard=True)
-    update.message.reply_photo(open('logo/logo.jpg', 'rb'), caption='Commands (buttons not working for now):',
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_photo(open('logo/logo.jpg', 'rb'), caption='Commands:',
                                reply_markup=reply_markup)
 
 
@@ -77,39 +85,100 @@ def check_stats(update: Update, context: CallbackContext):
     query = update.callback_query
     query.answer()
     keyboard = [
-        [InlineKeyboardButton("By Game", callback_data=str(FIRST))],
-        [InlineKeyboardButton("By Player", callback_data=str(SECOND))]
+        [InlineKeyboardButton("By Game", callback_data=str(THIRD))],
+        [InlineKeyboardButton("By Player", callback_data=str(FOURTH))]
     ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
+    reply_markup = InlineKeyboardMarkup(keyboard, one_time_keyboard=True)
     query.edit_message_reply_markup(reply_markup)
-
-    return ONE
+    return THREE
 
 
 def get_game_stats(update: Update, context: CallbackContext):
-    pass
+    msg = ''
+    if update.message.text.lower() in [i.get_Name().lower() for i in games_list]:
+        for i in games_list:
+            if update.message.text.lower().__eq__(i.get_Name()):
+                for key, val in i.get_users_stats().items():
+                    msg += '{} è arrivato '.format(key) + dyn_string(1, val, i.get_max_players()) + '\n'
+        if len(msg) >= 1:
+            update.message.reply_text(msg)
+        else:
+            update.message.reply_text('Non ci sono dati per ora')
+    else:
+        update.message.reply_text('Non ci sono giochi in lista chiamati {}'.format(update.message.text))
+    # update.message.delete()
+    return ConversationHandler.END
 
 
 def get_player_stats(update: Update, context: CallbackContext):
-    pass
+    update.message.delete()
+    return ConversationHandler.END
 
 
 def select_game(update: Update, context: CallbackContext):
     query = update.callback_query
     query.answer()
     query.message.reply_text('Inserire Nome Del Gioco')
-    return TWO
+    query.message.delete()
+    return FOUR
 
 
 def select_player(update: Update, context: CallbackContext):
     query = update.callback_query
     query.answer()
     query.message.reply_text('Inserire Nome Del Giocatore')
-    return THREE
+    query.message.delete()
+    return FIVE
+
+
+def select_update_game(update: Update, context: CallbackContext):
+    global tmp_game
+    if update.message.text.lower() in [i.get_Name().lower() for i in games_list]:
+        for j in games_list:
+            if j.get_Name().lower():
+                tmp_game[0] = j
+        update.message.reply_text('inserire nome giocatore e posizione separati da spazio (e.g. Mario 1)')
+        update.message.delete()
+        return SEVEN
+    else:
+        update.message.reply_text('nessun gioco trovato col nome {}'.format(update.message.text))
+        update.message.delete()
+        return ConversationHandler.END
+
+
+def upd_name(update: Update, context: CallbackContext):
+    query = update.callback_query
+    query.answer()
+    query.message.reply_text('Inserire Nome Del Gioco')
+    query.message.delete()
+    return SIX
 
 
 def quit_conversation(update: Update, context: CallbackContext):
     return ConversationHandler.END
+
+
+def update_game(update: Update, context: CallbackContext):
+    global tmp_game
+    msg = update.message.text.split()
+    if len(msg) < 2:
+        tmp_game = [' ', ' ']
+        return ConversationHandler.END
+    else:
+        tmp_game[0].update_user(msg[0], int(msg[1]))
+        update.message.reply_text('inserire il successivo nome giocatore e posizione separati da spazio (e.g. Mario 1)')
+        update.message.delete()
+        return SEVEN
+
+
+def show_games(update: Update, context: CallbackContext):
+    msg = ''
+    query = update.callback_query
+    query.answer()
+    for i in games_list:
+        msg += i.get_Name() + '\n'
+    query.message.reply_text(msg)
+    query.message.delete()
 
 
 def main() -> None:
@@ -121,32 +190,28 @@ def main() -> None:
         print(e)
         return
     dispatcher = bot_updater.dispatcher
-    dispatcher.add_handler(CommandHandler("stats", get_game_stats))
-    # dispatcher.add_handler(CommandHandler("upd", update_game))
+    dispatcher.add_handler(CommandHandler("commands", commands))
 
     dispatcher.add_handler(ConversationHandler(
-        entry_points=[CallbackQueryHandler(check_game, pattern='^' + str(FIRST) + '$')],
+        entry_points=[CallbackQueryHandler(check_game, pattern='^' + str(FIRST) + '$'),
+                      CallbackQueryHandler(check_stats, pattern='^' + str(SECOND) + '$'),
+                      CallbackQueryHandler(upd_name, pattern='^' + str(FIFTH) + '$'),
+                      CallbackQueryHandler(show_games, pattern='^' + str(SIXTH) + '$')],
         states={
             ONE: [MessageHandler(Filters.text, callback=check_size)],
-            TWO: [MessageHandler(Filters.text, callback=insert_game)]
-
-        },
-        fallbacks=[CommandHandler('quit', quit_conversation)]
-    ))
-    dispatcher.add_handler(ConversationHandler(
-        entry_points=[CallbackQueryHandler(check_stats, pattern='^' + str(SECOND) + '$')],
-        states={
-            ONE: [
-                CallbackQueryHandler(select_game, pattern='^' + str(FIRST) + '$'),
-                CallbackQueryHandler(select_player, pattern='^' + str(SECOND) + '$'),
+            TWO: [MessageHandler(Filters.text, callback=insert_game)],
+            THREE: [
+                CallbackQueryHandler(select_game, pattern='^' + str(THIRD) + '$'),
+                CallbackQueryHandler(select_player, pattern='^' + str(FOURTH) + '$'),
             ],
-            TWO: [MessageHandler(Filters.text, callback=get_game_stats)],
-            THREE: [MessageHandler(Filters.text, callback=get_player_stats)]
+            FOUR: [MessageHandler(Filters.text, callback=get_game_stats)],
+            FIVE: [MessageHandler(Filters.text, callback=get_player_stats)],
+            SIX: [MessageHandler(Filters.text, callback=select_update_game)],
+            SEVEN: [MessageHandler(Filters.text, callback=update_game)]
 
         },
         fallbacks=[CommandHandler('quit', quit_conversation)]
     ))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, commands))
     global games_list
     try:
         if os.stat("data.pickle").st_size > 0:
@@ -154,6 +219,22 @@ def main() -> None:
     except Exception as e:
         print(e)
         return
+    games_list.append(Game('test', 4))
+    games_list[0].update_user('capuz', 1)
+    games_list[0].update_user('rick', 2)
+    games_list[0].update_user('boss', 3)
+    games_list[0].update_user('capuz', 1)
+    games_list[0].update_user('rick', 3)
+    games_list[0].update_user('boss', 2)
+    games_list[0].update_user('capuz', 1)
+    games_list[0].update_user('rick', 2)
+    games_list[0].update_user('boss', 3)
+    games_list[0].update_user('capuz', 1)
+    games_list[0].update_user('rick', 2)
+    games_list[0].update_user('boss', 3)
+    games_list.append(Game('azz', 5))
+    games_list.append(Game('the mind', 4))
+    games_list.append(Game('surviving mars', 9))
     bot_updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
--- Game.py
+++ Game.py
@@ -1,7 +1,10 @@
+from copy import deepcopy
+
+
 class Game(object):
     def __init__(self, name, max_players=4):
         self.name = name
-        self.max_players = max_players
+        self.max_players = int(max_players)
         # dizionario con chiave = a nome e valore = a lista con le posizioni
         # e.g. 'rick': [0,0,2,1,0]: significa che non è mai arrivaot primo o secondo o quinto, due volte 3 e una volta 4
         self.users = dict()
@@ -9,18 +12,23 @@ class Game(object):
     def add_user(self, name):
         self.users[name] = [0] * self.max_players
 
-    def get_user_stats(self, name):
+    def get_user_stats(self, name) -> list:
         if name in self.users.keys():
             return self.users[name]
         else:
             return None
 
-    def get_users_stats(self):
-        return
+    def get_users_stats(self) -> dict:
+        return deepcopy(self.users)
+
     def update_user(self, name, position):
         if name not in self.users.keys():
             self.add_user(name)
-        self.users[name][position] = self.users[name][position] + 1
+        if position<=self.max_players:
+            self.users[name][position-1] = self.users[name][position-1] + 1
 
-    def get_Name(self):
+    def get_Name(self) -> str:
         return self.name
+
+    def get_max_players(self) -> int:
+        return self.max_players
